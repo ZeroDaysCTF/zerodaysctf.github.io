c9a4cf4447114a10d1a8b2e8aa33c0da87401781
Cillian Collins <cillian@ring0.localhost>
2025-01-07 22:13:36 +0000

    Fixing colleges

---
+++ js/config2.js
@@ -0,0 +1,23 @@
+/******************************************************************************************************************/
+/* CONFIG                                                                                                         */
+/******************************************************************************************************************/
+
+/* Edit the array as you wish. It will contain the typed strings. */
+var config_typedArray = ["March 23rd, 2024,  Croke Park, Dublin", "Compete, Learn, Have Fun!", "Is Cyber-Security in your future?"];
+
+/* Disable Parallax effect on touch devices for performance reasons. */
+var config_disableParalaxOnTouchDevices = true;
+
+/* Front page effect - can be 'none', 'particles', 'particle-fountain', 'video'  */
+var config_frontPageEffect = 'video';
+
+/* If config_frontPageEffect is set to video, here set the video URL, type and opacity */
+var config_videoUrl = 'video/background2.mp4';
+var config_videoType = 'video/mp4';
+var config_videoOpacity = 0.5;
+
+/* Google map parameters */
+var config_mapLat =  53.36113536762965; // latitude ,  
+var config_mapLng = -6.252308442329511; // longitude , 
+var config_mapZoom = 16; // map zoom
+var config_mapTitle = 'ZeroDays CTF 2024,  Croke Park, Dublin' // marker title
